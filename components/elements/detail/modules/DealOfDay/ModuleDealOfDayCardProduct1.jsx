61d484caf4fb45285d0b0b889ac173ec1ee25659
bigbasket-frontend <frontend@estrrado.com>
2022-11-16 19:06:42 +0530

    homepage design

--- components/elements/detail/modules/DealOfDay/ModuleDealOfDayCardProduct1.jsx
+++ components/elements/detail/modules/DealOfDay/ModuleDealOfDayCardProduct1.jsx
@@ -20,15 +20,9 @@ const ModuleDealOfDayCardProduct1 = ({ product }) => {
           <ProductThumbnail imageLink={product.image} />
         </a>
       </Link>
-      <div className="ps-product__container text-truncate text-center">
+      <div className="ps-product__container text-truncate text-center pt-3 pb-3">
         <Link href={`/shop?category=${product.id}`}>
-          <a
-            className="ps-product__title text-capitalize"
-            style={{
-              fontSize: "medium",
-              fontWeight: "500",
-            }}
-          >
+          <a className="ps-product__title text-capitalize">
             {product.category_name}
           </a>
         </Link>
